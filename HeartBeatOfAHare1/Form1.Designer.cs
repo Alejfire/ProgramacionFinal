b5dc7967d4581e41bb0e58ac0b80dee212da2b3f
Alejfire <alejandro.gomez.escoto@gmail.com>
2024-04-13 00:46:21 -0400

    first commit

---
+++ HeartBeatOfAHare1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace HeartBeatOfAHare1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.moveObjectTimer = new System.Windows.Forms.Timer(this.components);
+            this.rokk = new System.Windows.Forms.PictureBox();
+            this.rokk2 = new System.Windows.Forms.PictureBox();
+            this.grasss4 = new System.Windows.Forms.PictureBox();
+            this.grasss3 = new System.Windows.Forms.PictureBox();
+            this.grasss2 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.grasss = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rokk4 = new System.Windows.Forms.PictureBox();
+            this.rokk3 = new System.Windows.Forms.PictureBox();
+            this.RightMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.LeftMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.DownMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.UpMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.rokk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rokk2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grasss4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grasss3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grasss2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grasss)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rokk4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rokk3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // moveObjectTimer
+            // 
+            this.moveObjectTimer.Enabled = true;
+            // 
+            // rokk
+            // 
+            this.rokk.BackColor = System.Drawing.Color.Transparent;
+            this.rokk.Image = global::HeartBeatOfAHare1.Properties.Resources.rokk;
+            this.rokk.Location = new System.Drawing.Point(81, 293);
+            this.rokk.Name = "rokk";
+            this.rokk.Size = new System.Drawing.Size(32, 32);
+            this.rokk.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.rokk.TabIndex = 7;
+            this.rokk.TabStop = false;
+            // 
+            // rokk2
+            // 
+            this.rokk2.BackColor = System.Drawing.Color.Transparent;
+            this.rokk2.Image = global::HeartBeatOfAHare1.Properties.Resources.rokk;
+            this.rokk2.Location = new System.Drawing.Point(730, 207);
+            this.rokk2.Name = "rokk2";
+            this.rokk2.Size = new System.Drawing.Size(32, 32);
+            this.rokk2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.rokk2.TabIndex = 6;
+            this.rokk2.TabStop = false;
+            // 
+            // grasss4
+            // 
+            this.grasss4.BackColor = System.Drawing.Color.Transparent;
+            this.grasss4.Image = global::HeartBeatOfAHare1.Properties.Resources.grasssss__1_;
+            this.grasss4.Location = new System.Drawing.Point(1403, 293);
+            this.grasss4.Name = "grasss4";
+            this.grasss4.Size = new System.Drawing.Size(32, 32);
+            this.grasss4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.grasss4.TabIndex = 5;
+            this.grasss4.TabStop = false;
+            // 
+            // grasss3
+            // 
+            this.grasss3.BackColor = System.Drawing.Color.Transparent;
+            this.grasss3.Image = global::HeartBeatOfAHare1.Properties.Resources.grasssss__1_;
+            this.grasss3.Location = new System.Drawing.Point(999, 43);
+            this.grasss3.Name = "grasss3";
+            this.grasss3.Size = new System.Drawing.Size(32, 32);
+            this.grasss3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.grasss3.TabIndex = 4;
+            this.grasss3.TabStop = false;
+            // 
+            // grasss2
+            // 
+            this.grasss2.BackColor = System.Drawing.Color.Transparent;
+            this.grasss2.Image = global::HeartBeatOfAHare1.Properties.Resources.grasssss__1_;
+            this.grasss2.Location = new System.Drawing.Point(249, 261);
+            this.grasss2.Name = "grasss2";
+            this.grasss2.Size = new System.Drawing.Size(32, 32);
+            this.grasss2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.grasss2.TabIndex = 3;
+            this.grasss2.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::HeartBeatOfAHare1.Properties.Resources.oie_8714414KqVAM7S;
+            this.player.Location = new System.Drawing.Point(1257, 170);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(111, 52);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // grasss
+            // 
+            this.grasss.BackColor = System.Drawing.Color.Transparent;
+            this.grasss.Image = global::HeartBeatOfAHare1.Properties.Resources.grasssss__1_;
+            this.grasss.Location = new System.Drawing.Point(175, 70);
+            this.grasss.Name = "grasss";
+            this.grasss.Size = new System.Drawing.Size(32, 32);
+            this.grasss.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.grasss.TabIndex = 1;
+            this.grasss.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HeartBeatOfAHare1.Properties.Resources.pixilart_drawing;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -8);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(1600, 400);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(1600, 400);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1600, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rokk4
+            // 
+            this.rokk4.BackColor = System.Drawing.Color.Transparent;
+            this.rokk4.Image = global::HeartBeatOfAHare1.Properties.Resources.rokk;
+            this.rokk4.Location = new System.Drawing.Point(431, 12);
+            this.rokk4.Name = "rokk4";
+            this.rokk4.Size = new System.Drawing.Size(32, 32);
+            this.rokk4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.rokk4.TabIndex = 9;
+            this.rokk4.TabStop = false;
+            // 
+            // rokk3
+            // 
+            this.rokk3.BackColor = System.Drawing.Color.Transparent;
+            this.rokk3.Image = global::HeartBeatOfAHare1.Properties.Resources.rokk;
+            this.rokk3.Location = new System.Drawing.Point(1456, 147);
+            this.rokk3.Name = "rokk3";
+            this.rokk3.Size = new System.Drawing.Size(32, 32);
+            this.rokk3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.rokk3.TabIndex = 8;
+            this.rokk3.TabStop = false;
+            // 
+            // RightMoveTimer
+            // 
+            this.RightMoveTimer.Enabled = true;
+            // 
+            // LeftMoveTimer
+            // 
+            this.LeftMoveTimer.Enabled = true;
+            // 
+            // DownMoveTimer
+            // 
+            this.DownMoveTimer.Enabled = true;
+            // 
+            // UpMoveTimer
+            // 
+            this.UpMoveTimer.Enabled = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1582, 353);
+            this.Controls.Add(this.rokk4);
+            this.Controls.Add(this.rokk3);
+            this.Controls.Add(this.rokk);
+            this.Controls.Add(this.rokk2);
+            this.Controls.Add(this.grasss4);
+            this.Controls.Add(this.grasss3);
+            this.Controls.Add(this.grasss2);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.grasss);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1600, 400);
+            this.MinimumSize = new System.Drawing.Size(1600, 400);
+            this.Name = "Form1";
+            this.Text = "Heartbeat of a Hare";
+            this.Load += new System.EventHandler(this.HeartBeatofaHare_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rokk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rokk2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grasss4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grasss3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grasss2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grasss)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rokk4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rokk3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox grasss;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox grasss2;
+        private System.Windows.Forms.PictureBox grasss4;
+        private System.Windows.Forms.PictureBox grasss3;
+        private System.Windows.Forms.PictureBox rokk2;
+        private System.Windows.Forms.PictureBox rokk;
+        private System.Windows.Forms.PictureBox rokk4;
+        private System.Windows.Forms.PictureBox rokk3;
+        private System.Windows.Forms.Timer RightMoveTimer;
+        private System.Windows.Forms.Timer LeftMoveTimer;
+        private System.Windows.Forms.Timer DownMoveTimer;
+        private System.Windows.Forms.Timer UpMoveTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer moveObjectTimer;
+    }
+}
+
---
+++ HeartBeatOfAHare1/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HeartBeatOfAHare1
+{
+    public partial class Form1 : Form
+    {
+        int objectSpeed;
+
+        int leftPlayerSpeed;
+        int rightPlayerSpeed;
+        int verticalPlayerSpeed;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = @"C:\Users\aleja\Downloads\Launchpad Projects\Clean Bandit - Solo Project\Clean Bandit - Solo Project\Heartbeat of a harefinal.wav";
+            player.Play();
+
+            this.player.Parent = pictureBox1;
+            this.grasss.Parent = pictureBox1;
+            this.grasss2.Parent = pictureBox1;
+            this.grasss3.Parent = pictureBox1;
+            this.grasss4.Parent = pictureBox1;
+            this.rokk.Parent = pictureBox1;
+            this.rokk2.Parent = pictureBox1;
+            this.rokk3.Parent = pictureBox1;
+            this.rokk4.Parent = pictureBox1;
+
+            leftPlayerSpeed = 12;
+            verticalPlayerSpeed = 12;
+            rightPlayerSpeed = 24;
+            objectSpeed = 50;
+            moveObjectTimer.Start();
+        }
+
+        private void LeftMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (player.Left > 5)
+            {
+                player.Left -= leftPlayerSpeed;
+            }
+        }
+
+        private void RightMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (player.Right < 1595)
+            {
+                player.Left += rightPlayerSpeed;
+            }
+        }
+
+        private void DownMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (player.Bottom < 395)
+            {
+                player.Top += verticalPlayerSpeed;
+            }
+        }
+
+        private void UpMoveTimer_Tick(object sender, EventArgs e)
+        {
+            if (player.Top > 5)
+            {
+                player.Top -= verticalPlayerSpeed;
+            }
+        }
+
+        private void moveObjectTimer_Tick(object sender, EventArgs e)
+        {
+            Rectangle playerBounds = player.Bounds;
+
+            Rectangle[] obstacleBoundsArray = new Rectangle[]
+            {
+                rokk.Bounds,
+                rokk2.Bounds,
+                rokk3.Bounds,
+                rokk4.Bounds
+            };
+
+            foreach (Rectangle obstacleBounds in obstacleBoundsArray)
+            {
+                if (playerBounds.IntersectsWith(obstacleBounds))
+                {
+                    Application.Exit();
+                    break;
+                }
+            }
+
+            if (grasss.Right < 1595)
+            {
+                grasss.Left += objectSpeed;
+            }
+            else
+            {
+                grasss.Left = 0;
+            }
+
+            if (grasss2.Right < 1595)
+            {
+                grasss2.Left += objectSpeed;
+            }
+            else
+            {
+                grasss2.Left = 10;
+            }
+
+            if (rokk.Right < 1595)
+            {
+                rokk.Left += objectSpeed;
+            }
+            else
+            {
+                rokk.Left = 90;
+            }
+
+            if (rokk2.Right < 1595)
+            {
+                rokk2.Left += objectSpeed;
+            }
+            else
+            {
+                rokk2.Left = 40;
+            }
+
+            if (rokk3.Right < 1595)
+            {
+                rokk3.Left += objectSpeed;
+            }
+            else
+            {
+                rokk3.Left = 70;
+            }
+
+            if (rokk4.Right < 1595)
+            {
+                rokk4.Left += objectSpeed;
+            }
+            else
+            {
+                rokk4.Left = 60;
+            }
+
+            if (grasss3.Right < 1595)
+            {
+                grasss3.Left += objectSpeed;
+            }
+            else
+            {
+                grasss3.Left = 2;
+            }
+
+            if (grasss4.Right < 1595)
+            {
+                grasss4.Left += objectSpeed;
+            }
+            else
+            {
+                grasss4.Left = 20;
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                LeftMoveTimer.Start();
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                RightMoveTimer.Start();
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                DownMoveTimer.Start();
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                UpMoveTimer.Start();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            LeftMoveTimer.Stop();
+            RightMoveTimer.Stop();
+            DownMoveTimer.Stop();
+            UpMoveTimer.Stop();
+        }
+
+        //No utilizados --------------------------------------------|
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { }
+        private void player_Click(object sender, EventArgs e)
+        { }
+        private void grasss_Click(object sender, EventArgs e)
+        { }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        { }
+        private void Form1_load(object sender, EventArgs e)
+        { }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { }
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        { }
+        private void Form1_Load(object sender, EventArgs e) 
+        { }
+        private void HeartBeatofaHare_Load(object sender, EventArgs e)
+        { }
+    }
+}
